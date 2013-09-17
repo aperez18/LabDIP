0e837ab1b3af730aac7633fb4684788a84b6717c
Andy Perez <aperez18@my.wctc.edu>
2013-09-16 22:40:55 -0500

    lab3 framework started

---
+++ src/dip/lab3/student/solution1/KeyboardInput.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dip.lab3.student.solution1;
+
+import java.util.Scanner;
+/**
+ *
+ * @author Andy
+ */
+public class KeyboardInput {
+    
+}
---
+++ src/dip/lab3/student/solution1/Startup.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dip.lab3.student.solution1;
+
+/**
+ *
+ * @author Andy
+ */
+public class Startup {
+    public static void main(String[] args) {
+        //JOptionPane.showMessageDialog(null,"Sample String");
+        //String response = JOptionPane.showInputDialog(null, "A Prompt, like enter message here:");
+    }
+}
